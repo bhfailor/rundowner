f3ef5c050db819479b78c756e240fee76328004e
Bruce Failor <bruce.failor@gmail.com>
2015-06-10 16:20:48 +0000

    add metadata to tests and examples of different ways to run

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -1,19 +1,31 @@
 require 'test_helper'
-require 'minitest/metadata' # I think spec DSL is needed for metadata
+require 'minitest/metadata'
 
 class CanAccessHomeTest < Capybara::Rails::TestCase
-  include MiniTest::Metadata # I think spec DSL is needed for metadata
+
   test 'sanity' do
     visit root_path
     assert_content page, 'Welcome Participant'
     refute_content page, 'Goobye All!'
   end
-  test 'seeing the alert popup' do
-    Capybara.current_driver = :selenium # default javascript driver
+
+  # want to repeat this a number of times
+  def confirm_js_operation
+    assert Capybara.current_driver, :selenium # default javascript driver
     visit root_path
     alert = page.driver.browser.switch_to.alert
     assert alert.text, 'welcome#index'
-    Capybara.current_driver = Capybara.default_driver
+  end
+  
+  # the following should all be equivalent  
+  it 'sees the alert popup', :js do
+    confirm_js_operation
+  end
+  it 'sees the alert popup', :js => true do
+    confirm_js_operation
+  end
+  scenario 'the alert popup is seen', :js do
+    confirm_js_operation
   end
   
 end
